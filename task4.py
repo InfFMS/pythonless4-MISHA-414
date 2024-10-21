c37a55ed5839518eeaa3aa2214b7103bd7e64102
Петрухин Михаил Дмитриевич <m.petrukhin@main.nsu.ru>
2024-10-21 12:55:17 +0700

    task4

--- task4.py
+++ task4.py
@@ -3,5 +3,9 @@
 # На входе два числа, на выходе их НОД.
 
 a, b=map(int, input().split())
-ma=max(a, b)//2 +1
-for i in range()
+
+maxim=1
+for num in range(1, max(a, b)+1):
+    if a%num==0 and b%num==0:
+       maxim=num
+print(maxim)
